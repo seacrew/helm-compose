d236bce47be30b98934f4530560fd620e13fe56e
tuunit <jan@larwig.com>
2023-01-17 23:29:21 +0100

    add new name, state and createNamespace config flags

--- internal/compose/types.go
+++ internal/compose/types.go
@@ -1,17 +1,20 @@
 package compose
 
 type Release struct {
-	Name         string                 `yaml:"name,omitempty"`
-	Chart        string                 `yaml:"chart,omitempty"`
-	ChartVersion string                 `yaml:"chartVersion,omitempty"`
-	Namespace    string                 `yaml:"namespace,omitempty"`
-	KubeContext  string                 `yaml:"kubeContext,omitempty"`
-	Values       map[string]interface{} `yaml:"values,omitempty"`
-	ValueFiles   []string               `yaml:"valueFiles,omitempty"`
+	Name            string                      `yaml:"name,omitempty"`
+	Chart           string                      `yaml:"chart,omitempty"`
+	ChartVersion    string                      `yaml:"chartVersion,omitempty"`
+	Namespace       string                      `yaml:"namespace,omitempty"`
+	CreateNamespace bool                        `yaml:"createNamespace,omitempty"`
+	KubeContext     string                      `yaml:"kubeContext,omitempty"`
+	Values          map[interface{}]interface{} `yaml:"values,omitempty"`
+	ValueFiles      []string                    `yaml:"valueFiles,omitempty"`
 }
 
 type Config struct {
 	Version      string             `yaml:"composeVersion,omitempty"`
+	Name         string             `yaml:"name,omitempty"`
+	State        string             `yaml:"state,omitempty"`
 	Releases     map[string]Release `yaml:"releases,omitempty"`
 	Repositories map[string]string  `yaml:"repositories,omitempty"`
 }
